bbd4388f26654c214360410b40745da0b22ae6af
azb0180 <azb0180@auburn.edu>
2022-06-17 10:52:43 -0500

    seeed rp2040 migration

---
+++ Software/devices/seeed_read_rp2040/build/CMakeFiles/seeed_read_rp2040.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for seeed_read_rp2040.
+# This may be replaced when dependencies are built.
---
+++ Software/devices/seeed_read_rp2040/build/CMakeFiles/seeed_read_rp2040.dir/compiler_depend.ts
@@ -0,0 +1,2 @@
+# CMAKE generated file: DO NOT EDIT!
+# Timestamp file for compiler generated dependencies management for seeed_read_rp2040.
